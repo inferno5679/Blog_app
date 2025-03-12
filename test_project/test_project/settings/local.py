ad9fd925af18ad5f442e5d8658de384696e674e6
Arpan Doogar <arpandoogar@gmail.com>
2025-03-12 23:28:37 +0530

    added Login_redirect url for successful user authentication

--- test_project/test_project/settings/local.py
+++ test_project/test_project/settings/local.py
@@ -124,6 +124,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "blog-home"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
